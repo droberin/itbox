62a89bb7e27bb3560cd4bfe081633e6d65180e3c
Roberto Salgado Ladrero <rsalgado@gradiant.org>
2018-02-16 19:15:30 +0100

    initial commit

---
+++ itbox/__init__.py
@@ -0,0 +1,31 @@
+import requests
+import logging
+from configobj import ConfigObj
+from os.path import isfile
+
+def list_nodes(from_file='/var/lib/nova/itbox-nodes.ini'):
+    try:
+        with open(from_file, 'rb') as node_list_fp:
+
+
+
+class ITBox:
+    """"
+    ITBox class will create a whole new Cloud for you
+    """
+    cloud_initiated = False
+    provisioning_server = 'https://itbox.gradiant.org'
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialiser
+        :param args:
+        :param kwargs:
+        """
+        super(ITBox, self).__init__(*args, **kwargs)
+
+    def provision_server(self, url=None):
+        if not url:
+            logging.info("No URL requested. Using default provisioning server: " + self.provisioning_server)
+        else:
+            self.provisioning_server = url
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='itbox',
+    version='0.1.0',
+    packages=['itbox'],
+    url='https://g',
+    download_url='g',
+    license='',
+    author='The ITBox Team',
+    author_email='gradiant@gradiant.org',
+    description='ITBox Cloud Computing Utils'
+)
